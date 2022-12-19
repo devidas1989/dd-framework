74801277a4436c8eb9fad5548916764f9031c74e
unknown <devidasb81@gmail.com>
2022-12-19 08:21:44 +0530

    demo1 new code added

--- src/loginPage/demo1.java
+++ src/loginPage/demo1.java
@@ -4,7 +4,14 @@ public class demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("demo1 class created");
+	System.out.println("repository");
+	
+	
+	
+	
+	
+	
 	}
 
 }
