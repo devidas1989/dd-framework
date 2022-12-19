d0ca93c5484f0e807de2cb0876c449e51a40883f
unknown <devidasb81@gmail.com>
2022-12-19 07:53:16 +0530

    base class created

---
+++ src/loginPage/BaseClass.java
@@ -0,0 +1,11 @@
+package loginPage;
+
+public class BaseClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("base class created");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vishal bhosale
+ *
+ */
+module dd {
+}
